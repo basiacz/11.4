fe4f236703b389bf1bf7513ecb89cb281c6bcb31
basiacz <basiaczerw@gmail.com>
2018-12-12 22:14:53 +0100

    11.4

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function Phone(brand, price, color) {
   this.brand = brand;
   this.price = price;
   this.color = color;
-};
+}
 
 Phone.prototype.printInfo = function () {
   console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-};
+}
 
 var SamsungGalaxy6 = new Phone("Samsung", 3500, "White");
 var iPhoneXs = new Phone("Apple", 7219, "Gold");
